a8a9cfbccf980aa936ed49770e70dd8372f8e82f
freetes <www.1030415864@qq.com>
2018-06-19 20:43:18 +0800

    fix local server

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+let html = ''
+
 app.use('/public', express.static('public'));
 app.use('/config', express.static('config'));
+app.use('/articles', express.static('articles'));
 
 app.get('/', (req, res) => {
-  fs.readFile('./index.html', 'utf-8', data=>{
-    return res.end(data)
-  })
+  if(html != ''){
+    return res.end(html)
+  }
+  else{
+    fs.readFile('./index.html', 'utf-8', (err, data)=>{
+      html = data
+      return res.end(html)
+    })
+  }
 });
 
 const server = app.listen(3000, function () {
