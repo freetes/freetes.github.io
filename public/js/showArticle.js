6079235f2b661fb9ce7e2f55112984e3e784e59c
freetes <www.1030415864@qq.com>
2018-06-20 20:14:37 +0800

    update ajax

--- public/js/showArticle.js
+++ public/js/showArticle.js
@@ -1,6 +1,22 @@
 const webConfigLink = './config/webConfig.json',
       artConfigLink = './config/artConfig.json'
 
+// ajax Promise version
+function ajaxPromise(method, url) {
+  return new Promise((resolve, reject)=>{
+    $.ajax({
+      method: method || 'GET',
+      url: url,
+      success: res=>{
+        resolve(res)
+      },
+      error: res=>{
+        reject(res)
+      }
+    })
+  })
+}
+
 function showArticle(node, info){
   $(".list").children().removeClass()
   $(node).addClass("selected")
@@ -31,55 +47,65 @@ function showArticle(node, info){
   }
 }
 
-function createHtml(data) {
-  document.title = data.title
-  $("#blogName").text(data.title)
-  $("#subName").text(data.subtitle)
-  $("#endWords").text(data.end)
+function getWebConfig() {
+  return ajaxPromise('get', webConfigLink)
+}
+
+function getArtConfig() {
   return ajaxPromise('get', artConfigLink)
-  // ajaxPromise('get', artConfigLink).then()
 }
 
-// ajax Promise version
-function ajaxPromise(method, url) {
-  return new Promise((resolve, reject)=>{
-    $.ajax({
-      method: method || 'GET',
-      url: url,
-      success: res=>{
-        resolve(res)
-      },
-      error: res=>{
-        reject(res)
-      }
+function createArtList(data) {
+  let articles = data.articles
+  // 按时间排序
+  articles.sort((a, b)=>new Date(b.date)-new Date(a.date))
+  for(let item of articles){
+    let li = document.createElement("li")
+    li.id = `#${item.title}`
+    li.onclick = function () {
+      showArticle(this, item)
+    }
+    li.innerHTML = `
+      <p class='title'>${item.title}</p>
+      <span class='subtitle subtext'>${item.subtitle}</span>
+      <span class='date subtext'>写于${item.date}</span>
+    `
+    $(".list").append(li)
+  }
+  // 根据URL#后的参数找到对应的文章并点击
+  // 伪造搜索功能
+  let hash = decodeURIComponent(location.hash).split('#')[1]
+  if(decodeURIComponent(location.hash)){
+    for(let item of $(".list").children()){
+      if(item.id == decodeURIComponent(location.hash))
+        return $(item).click()
+    }
+  }
+  $(".list").children().first().click()
+}
+
+function createHtml() {
+  if(!localStorage.getItem('title') || !localStorage.getItem('subtitle') || !localStorage.getItem('end')){
+    getWebConfig().then(res=>{
+      // 保存缓存
+      localStorage.setItem('title', res.title)
+      localStorage.setItem('subtitle', res.subtitle)
+      localStorage.setItem('end', res.end)
+      document.title = res.title
+      $("#blogName").text(res.title)
+      $("#subName").text(res.subtitle)
+      $("#endWords").text(res.end)
     })
+  }
+  else{
+    document.title = localStorage.getItem('title')
+    $("#blogName").text(localStorage.getItem('title'))
+    $("#subName").text(localStorage.getItem('subtitle'))
+    $("#endWords").text(localStorage.getItem('end'))
+  }
+  getArtConfig().then(res=>{
+    createArtList(res)
   })
 }
 
-ajaxPromise('get', webConfigLink).then(res=>{
-  createHtml(res).then(res=>{
-    let articles = res.articles
-    articles.sort((a, b)=>new Date(b.date)-new Date(a.date))
-    for(let item of articles){
-      let li = document.createElement("li")
-      li.id = `#${item.title}`
-      li.onclick = function () {
-        showArticle(this, item)
-      }
-      li.innerHTML = `
-        <p class='title'>${item.title}</p>
-        <span class='subtitle subtext'>${item.subtitle}</span>
-        <span class='date subtext'>写于${item.date}</span>
-      `
-      $(".list").append(li)
-    }
-    let hash = decodeURIComponent(location.hash).split('#')[1]
-    if(decodeURIComponent(location.hash)){
-      for(let item of $(".list").children()){
-        if(item.id == decodeURIComponent(location.hash))
-          return $(item).click()
-      }
-    }
-    $(".list").children().first().click()
-  })
-})
+createHtml()
