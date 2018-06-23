91e76e7795e19c82196567e552bf0aeed5ce5aad
freetes <www.1030415864@qq.com>
2018-06-23 13:37:17 +0800

    fix padding bug

--- public/js/animation.js
+++ public/js/animation.js
@@ -3,9 +3,9 @@ const animateTime = 300
 function toggleList () {
   $('#blogName').attr('onclick', '')
   // 电脑端
-  if(screen.width > 800){
+  if(screen.width > 960){
     if($('.list').css('display') == 'none'){
-      $('.markdown-body').animate({width: '67%', padding: '8px 1%', margin: '0 1% 0 0'}, animateTime).promise().done(()=>{
+      $('.markdown-body').animate({width: '67%', padding: '20px 1%', margin: '0 1% 0 0'}, animateTime).promise().done(()=>{
         $('.list').toggle(animateTime).promise().done(()=>{
           $('#blogName').attr('onclick', 'toggleList()')
         })
@@ -13,7 +13,7 @@ function toggleList () {
     }
     else{
       $('.list').toggle(animateTime).promise().done(function (){
-        $('.markdown-body').animate({ width: '70%', padding: '8px 14%', margin: '0 1%'}, animateTime).promise().done(()=>{
+        $('.markdown-body').animate({ width: '70%', padding: '20px 14%', margin: '0 1%'}, animateTime).promise().done(()=>{
           $('#blogName').attr('onclick', 'toggleList()')
         })
       })
