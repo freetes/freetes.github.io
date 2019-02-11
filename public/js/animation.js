d96c26a6211e4f25ff913c6b4d5fbe787c86c8c3
freetes <www.1030415864@qq.com>
2019-02-11 17:42:15 +0800

    更新功能和样式

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,28 +1,28 @@
-const animateTime = 300
+const ANIMATION_TIME = 300
 
 function toggleList (node) {
-  $(node).attr('onclick', '')
+  $(node).removeAttr('onclick')
   // 电脑端
   if(screen.width > 960){
     if($('.list').css('display') == 'none'){
-      $('.markdown-body').animate({width: '100%', padding: '5px 10px', margin: '0 10px'}, animateTime).promise().done(()=>{
-        $('.list').toggle(animateTime).promise().done(()=>{
-          $(node).attr('onclick', 'toggleList()')
+      $('.markdown-body').animate({width: '100%', padding: '20px 10px', margin: '0 10px'}, ANIMATION_TIME).promise().done(()=>{
+        $('.list').toggle(ANIMATION_TIME).promise().done(()=>{
+          $(node).attr('onclick', 'toggleList(this)')
         })
       })
     }
     else{
-      $('.list').toggle(animateTime).promise().done(function (){
-        $('.markdown-body').animate({ width: '70%', padding: '20px 14%', margin: '0 1%'}, animateTime).promise().done(()=>{
-          $(node).attr('onclick', 'toggleList()')
+      $('.list').toggle(ANIMATION_TIME).promise().done(function (){
+        $('.markdown-body').animate({ width: '70%', padding: '20px 14%', margin: '0 10px'}, ANIMATION_TIME).promise().done(()=>{
+          $(node).attr('onclick', 'toggleList(this)')
         })
       })
     }
   }
   // 手机端
   else{
-    $('.list').slideToggle(animateTime).promise().done(()=>{
-      $(node).attr('onclick', 'toggleList()')
+    $('.list').slideToggle(ANIMATION_TIME).done(()=>{
+      $(node).attr('onclick', 'toggleList(this)')
     })
   }
 }
